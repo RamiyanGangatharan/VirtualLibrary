bbc5fc5e65d234a71d54ec1b6041cc669c2af6d7
Ramiyan Gangatharan <ramiyan2185@gmail.com>
2025-10-21 10:28:30 -0400

    added user object

---
+++ src/LibraryUser.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+
+public class LibraryUser {
+    String firstName;
+    String lastName;
+    String role;
+    int minValue = 100835223;
+    int maxValue = 999999999;
+    int bannerID = 0;
+
+    Random random = new Random();
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+    public int getBannerID() { return bannerID; }
+    public void setBannerID(int bannerID) { this.bannerID = bannerID; }
+
+    public LibraryUser()
+    {
+        this.firstName = "John";
+        this.lastName = "Doe";
+        this.bannerID = random.nextInt((maxValue - minValue + 1) + minValue); // generates a random num regardless of user
+        this.role = "MEMBER";
+    }
+
+    public LibraryUser(String firstName, String lastName, int bannerID, String role)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.bannerID = bannerID;
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("| %-15s | %-10s | %-10s |", firstName + " " + lastName, bannerID, role);
+    }
+}
--- src/startMenu.java
+++ src/startMenu.java
@@ -1,9 +1,62 @@
+import java.util.Scanner;
+
 public class startMenu {
-    // TODO: create a menu that will interface between the user and the software
-    public static void console()
+    // Enum for menu options
+    enum MenuOptions {
+        // =========================== OPTIONS =================================//
+        USER_CONFIGURATION("|| 1. USER CONFIGURATION"),
+        EXIT_APPLICATION("|| 0. EXIT APPLICATION");
+        // ======================================================================//
+
+        private final String text;
+        MenuOptions(String text) { this.text = text; }
+        public String getText() { return text; }
+    }
+
+    public static void console() {
+        Scanner scanner = new Scanner(System.in);
+        int input = -1;
+
+        while (input != 0) {
+            System.out.println("===================================================");
+            System.out.println("         Welcome to our Virtual Library            ");
+            System.out.println("===================================================");
+            System.out.println(MenuOptions.USER_CONFIGURATION.getText());
+            System.out.println(MenuOptions.EXIT_APPLICATION.getText());
+            System.out.println("===================================================");
+            System.out.print(">> ");
+
+            // get new input each loop
+            input = scanner.nextInt();
+
+            System.out.println("USER_INPUT: " + input);
+
+            // handle choices
+            switch (input) {
+                case 1:
+                    System.out.println("Opening User Configuration...");
+                    userConfig();
+                    break;
+                case 0: System.out.println("Exiting program. Goodbye!"); break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+                    break;
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static void userConfig()
     {
-        System.out.println("===================================================");
-        System.out.println("         Welcome to our Virtual Library            ");
-        System.out.println("===================================================");
+        LibraryUser user = new LibraryUser();
+
+        System.out.println("                       CURRENT USERS                          ");
+        System.out.println("==============================================================");
+        System.out.printf("| %-15s | %-10s | %-10s |%n", "Name", "Banner ID", "Role");
+        System.out.println("--------------------------------------------------------------");
+        System.out.println(user);
+        System.out.println("--------------------------------------------------------------");
+        System.out.println();
     }
 }
